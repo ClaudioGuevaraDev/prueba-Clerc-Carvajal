3db6d51cdf2cdcea7eb655b18afd04ec8b1eae34
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-10-12 15:07:39 -0300

    Calculo de la renta bruta lista

--- pregunta1/main.py
+++ pregunta1/main.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 df = pd.read_excel("./prueba.xlsx")
 
-print(df.columns)
+
+def calcular_renta(fila):
+    renta = fila["Sueldo Base Mensual $"] + fila["Gratificación Mensual $"] + fila["Asignación Almuerzo Mensual $"] + \
+        fila["Vales Almuerzo Valor Bruto Mensual $"] + \
+        fila["Valor Casino  Bruto Mensual $"] + \
+        fila["Asignación Movilización Mensual $"]
+
+    return renta
+
+
+df["Renta Bruta"] = df.apply(calcular_renta, axis=1)
+
+df.to_excel("prueba_con_renta_bruta.xlsx", index=False, header=True)
